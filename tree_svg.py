8a61df0bcd35556d0637d8045cfc6307b8d0bea5
Agizin <jonhom1996@gmail.com>
2017-10-29 16:41:47 -0400

    Now able to visualize any tree w/o overlap

--- tree_svg.py
+++ tree_svg.py
@@ -3,86 +3,109 @@ import svgwrite
 """
 Creates SVG file visualizing given tree
 
-Note that if there are too many nodes on a layer, they currently will overlap
+TODO: add values to tree nodes
 """
 
 class Tree():
-    def __init__(self, value, children = []):
+    def __init__(self, value, children = None):
         self.value = value
         self.children = children
+
+    def __str__(self):
+        return str(self.value)
 
     def add_child(t):
         self.children.append(t);
 
-def draw_tree(svg_doc, root, center): 
-    if root.children != []:
-        x = center[0]
-        y = center[1]
-        num_children = len(root.children)
-        child_centers = []
-        if num_children % 2 == 1:
-            mid = int(num_children/2)
-            for i in range(0, num_children):
-                child = root.children[i]
-                child_y = y + 200
-                if i <= mid:
-                    child_x = x - (mid-i)*110
-                else:
-                    child_x = x + (i-mid)*110
-                child_centers.append((child_x, child_y))
-        else:
-            half = int(num_children/2)-1
-            for i in range(0, num_children):
-                child = root.children[i]
-                child_y = y + 200
-                if i <= half:
-                    child_x = x - ((half-i)*110+55)
-                else:
-                    child_x = x + ((i-half-1)*110+55)
-                child_centers.append((child_x, child_y))
-        for i in range(0,num_children):
-            child = root.children[i]
-            child_center = child_centers[i]
-            child_top = (child_center[0], child_center[1] - 50)
-            child_edge = svg_doc.line(start = center,
-                                      end = child_top,
-                                      stroke_width = '3',
-                                      stroke='black')
-            svg_doc.add(child_edge)
-            draw_tree(svg_doc, child, child_center)
-            
-    node = svg_doc.circle(center = center,
-                          r = 50,
-                          stroke_width = '3',
-                          stroke ='black',
-                          fill='white')
-    svg_doc.add(node)
+class TreeVisualizer():
+    
+    def __init__(self, svg_doc):
+        self.svg_doc = svg_doc
+        self.nodes = []
+        self.width_dict = {}
 
-    if root.value:
-        text_str = str(root.value)
-        #text_insert_x = center[0]-10*(len(text_str)-1)
-        if len(text_str) <= 2:
-            text_size = '40px'
-            text_insert_x = center[0] - 12*len(text_str)
-        elif len(text_str) <= 10:
-            text_size = str(40 - 3*(len(text_str))) + 'px'
-            text_insert_x = center[0] - 9*len(text_str)
+    def __str__(self):
+        return self.svg_doc.tostring()
+
+    def save(self):
+        self.svg_doc.save()
+
+    def _width(self, root, radius, node_sep):
+        if root in self.width_dict:
+            return self.width_dict[root]
+        if root.children is None:
+            width = 2*radius + 2*node_sep
         else:
-            text_size = '10'
-        text_insert_y = center[1] + 10
+            width = 0
+            for c in root.children:
+               width += self._width(c, radius, node_sep)
+        self.width_dict[root] = width
+        return width
+
+    def _draw(self, parent_center, level_nodes, **kwargs):
+        radius = kwargs.get('radius', 50)
+        edge_length = kwargs.get('edge_length', 200)
+        node_sep = kwargs.get('node_sep', 10)
+
+        if level_nodes != None:
+            sub_widths = []
+            for subtree in level_nodes:
+                try:
+                    sub_width = self.width_dict[subtree]
+                except KeyError:
+                    sub_width = self._width(subtree, radius, node_sep)
+                sub_widths.append(sub_width)
+            level_length = sum(sub_widths)
+            y = parent_center[1]+edge_length
+            far_x_bound = parent_center[0] - level_length/2
+            for i in range(0,len(level_nodes)):
+                subtree = level_nodes[i]
+                sub_width = sub_widths[i]
+                x = far_x_bound + sub_width/2
+                far_x_bound = far_x_bound + sub_width
+                self._draw((x,y), subtree.children, **kwargs)
+                top = (x, y-radius)
+                edge = self.svg_doc.line(start = parent_center,
+                                         end = top,
+                                         stroke_width='3',
+                                         stroke='black')
+                self.svg_doc.add(edge)
+                
+        parent_node = self.svg_doc.circle(center = parent_center,
+                                          r = radius,
+                                          stroke_width='3',
+                                          stroke='black',
+                                          fill='white')
+        self.svg_doc.add(parent_node)
             
-        text = svg_doc.text(text_str,
-                            insert = (text_insert_x, text_insert_y),
-                            font_size=text_size)
-        svg_doc.add(text)
+            
+    def draw(self, root, center, **kwargs):
+        self._draw(center, root.children, **kwargs)
 
 svg_document = svgwrite.Drawing(filename ='tree.svg',
                                 size = ('600px','800px'))
 
 tree = Tree('A',[Tree('DE',[Tree('GHI')]),Tree('JKL'),Tree('MNOP',[Tree('PQRST'),Tree('STUV')])])
 
-draw_tree(svg_document, tree, (300, 100))
+tree_vis = TreeVisualizer(svg_document) 
 
-print(svg_document.tostring())
+tree_vis.draw(tree, (300, 100))
 
-svg_document.save()
+print(tree_vis)
+
+tree_vis.save()
+
+svg_doc2 = svgwrite.Drawing(filename='binary_tree.svg',
+                            size = ('600px', '800px'))
+
+
+tree2 = Tree('1', [Tree('2',[Tree('3'),Tree('4')]), Tree('5', [Tree('6'), Tree('7')])])
+
+tree_vis2 = TreeVisualizer(svg_doc2)
+
+tree_vis2.draw(tree2, (300,100))
+
+print()
+print(tree_vis2)
+
+tree_vis2.save()
