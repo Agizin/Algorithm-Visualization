8c2c46b0bf92a4da19a65f087b9f6b34bf47e15a
Oliver Kisielius <oliver.kisielius@gmail.com>
2018-02-03 19:52:44 -0500

    Just some code for elements of a picture.

---
+++ algviz/picture/elements.py
@@ -0,0 +1,93 @@
+# The elements that pictures can have
+
+# These picture elements don't hold information about their own location, since
+# they'll be passed around by objects with different internal coordinate
+# systems.
+
+from collections import namedtuple
+import enum
+
+_LOW = -1
+_MID = 0
+_HIGH = 1
+
+@enum.unique
+class Anchor(enum.Enum):
+    top_left = (_LOW, _LOW)
+    top = (_MID, _LOW)
+    top_right = (_HIGH, _LOW)
+    left = (_LOW, _MID)
+    center = (_MID, _MID)
+    right = (_HIGH, _MID)
+    bottom_left = (_LOW, _HIGH)
+    bottom = (_MID, _HIGH)
+    bottom_right = (_HIGH, _HIGH)
+
+Coord = namedtuple("Coord", ("x", "y"))
+
+def top_left_corner(rectangle, coord, anchor):
+    if not isinstance(anchor, Anchor):
+        # (Members of an Enum are also instances of that Enum.)
+        raise TypeError("{} is not an anchor.  Use an element of {}".format(anchor, Anchor))
+    def fix_one_dimension(given, anchor_val, side_length):
+        if anchor_val is _LOW:
+            return given
+        elif anchor_val is _HIGH:
+            return given - side_length
+        else:
+            return given - side_length / 2
+
+    x, y = coord
+    x_anch, y_anch = anchor.value
+    return Coord(fix_one_dimension(x, x_anch, rectangle.width),
+                 fix_one_dimension(y, y_anch, rectangle.height))
+
+
+class PictureElement(object):
+    # These are all things that will be laid out inside pictures so that they can be drawn.
+    pass
+
+class RectangularElement(PictureElement):
+    """Any element that takes up space.  For now, it gets a width and a height."""
+    def __init__(self, width, height, **kwargs):
+        self.width = width
+        self.height = height
+
+    def place(self, point):
+        self.location = point
+
+class NodeElement(RectangularElement):
+    """A node, which fits into a bounding box"""
+    # def __init__(self, width, height, **kwargs):
+    #     self.width = width
+    #     self.height = height
+    #     super().__init__(**kwargs)
+
+class PointerSource(RectangularElement):
+    """The start of a pointer, usually a big dot."""
+    def __init__(self, **kwargs):
+        width, height = kwargs["engine"].pointer_size
+        super().__init__(width, height, **kwargs)
+
+class StringElement(RectangularElement):
+    """A string, geez"""
+    def __init__(self, text, engine=None, **kwargs):
+        width, height = engine.string_size(text)
+        super().__init__(width, height, engine=engine, **kwargs)
+
+class Decoration(PictureElement):
+    """Stuff that gets tacked on but doesn't take up space"""
+    pass
+
+class Arrow(Decoration):
+    """Gets drawn as an arrow or line of some kind"""
+    def __init__(self, origin, destination, **kwargs):
+        self.origin = origin
+        self.destination = destination
+        super().__init__(**kwargs)
+
+class StraightArrow(Arrow):
+    pass
+
+class SplineArrow(Arrow):
+    pass
---
+++ algviz/picture/test_elements.py
@@ -0,0 +1,19 @@
+import unittest
+import collections
+from . import elements
+
+class AnchorTestCase(unittest.TestCase):
+    TestRectangle = collections.namedtuple("TestRectangle", ("width", "height"))
+
+    def test_bottom_right_corner_anchor(self):
+        # The top left corner of a rectangle of width 10 and height 6, with its
+        # bottom right corner at (0, 0), would be (-10, -6).
+        self.assertEqual(tuple(elements.top_left_corner(
+            self.TestRectangle(width=10, height=6),
+            (0, 0),
+            elements.Anchor.bottom_right)),
+                         (-10, -6))
+                         
+            
+if __name__ == "__main__":
+    unittest.main()
