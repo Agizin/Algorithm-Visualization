a85faaf091f3d18749a023e116917983d8f293df
Agizin <jonhom1996@gmail.com>
2018-03-29 19:46:48 -0400

    Adding command line tool to generate svg from json

---
+++ algviz/tools/make_picture.py
@@ -0,0 +1,14 @@
+import sys
+
+from algviz.parser import json_objects
+from algviz.parser import picture
+
+json = sys.argv[1]
+if len(ays.argv) > 2:
+    outfile = sys.argv[2]
+else:
+    outfile = "picture.svg"
+    
+structure = json_objects.decode_json(text)[0] #assumes only given single snapshot
+pic = picture.Picture.make_picture(structure, filename=outfile)
+pic.draw()
