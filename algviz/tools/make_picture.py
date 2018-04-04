a632a262eb2e3e3407ea3f4d793e846713523f46
Agizin <jonhom1996@gmail.com>
2018-04-04 16:18:22 -0400

    fixing command line tool for picture drawing

--- algviz/tools/make_picture.py
+++ algviz/tools/make_picture.py
@@ -3,12 +3,22 @@ import sys
 from algviz.parser import json_objects
 from algviz.picture import picture
 
-json = sys.argv[1]
-if len(ays.argv) > 2:
-    outfile = sys.argv[2]
-else:
-    outfile = "picture.svg"
-    
-structure = json_objects.decode_json(text)[0] #assumes only given single snapshot
-pic = picture.Picture.make_picture(structure, filename=outfile)
-pic.draw()
+def make_picture_from_json(json_file, object_uid, outfile):
+    json_contents = None
+    with open(json_file,'r') as f:
+        json_contents = f.read()
+    assert(json_contents is not None)
+    snapshot = json_objects.decode_json(json_contents)[0] #assumes only given single snapshot
+    structure = snapshot.obj_table.getuid(object_uid)
+    pic = picture.Picture.make_picture(structure)
+    pic.draw()
+    pic.save(outfile)
+
+if __name__ == "__main__":
+    json_file = sys.argv[1]
+    uid = sys.argv[2]
+    if len(sys.argv) > 3:
+        outfile = sys.argv[3]
+    else:
+        outfile = "picture.svg"
+    make_picture_from_json(json_file,uid,outfile)
