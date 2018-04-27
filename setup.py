3cc63b93477b5fe1074f54b489f8e9db72ff2de9
Oliver Kisielius <oliver.kisielius@gmail.com>
2018-04-27 10:32:46 -0400

    added tool to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='algviz',
               "algviz_graph_mockup=algviz.tools.graph_drawing_mockup:main",
               "algviz_quicksort_example=algviz.tools.quicksort_tree:main",
               "algviz_rec_draw=algviz.tools.draw_recursive_pic:main",
+              "algviz_random_tree=algviz.tools.bin_tree_maker:main",
           ]},
       install_requires=[
           'pygraphviz',
